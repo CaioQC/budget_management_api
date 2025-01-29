948873ee01aca04e0bde3ba27131e721bf4e3ca8
CaioQC <caioq2004@gmail.com>
2025-01-29 13:13:28 -0300

    "Created summary route to show the total revenues and expenses amount by year and month"

--- app/controllers/budget_summaries_controller.ts
+++ app/controllers/budget_summaries_controller.ts
@@ -1,29 +1,50 @@
-import Expense from '#models/expense'
+import ExpensesCategory from '#models/expenses_category';
 import Revenue from '#models/revenue'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class BudgetSummariesController {
     async summary({response, params}:HttpContext){
         const { year, month } = params
+        const categoriesQuery = await ExpensesCategory.query().preload('expenses', (query) => {
+            query
+                .whereRaw("TO_CHAR(expense_date, 'YYYY') = ?", [year])
+                .whereRaw("TO_CHAR(expense_date, 'MM') = ?", [month]);
+        });
 
-        const queryExpensesByYearAndMonth = await Expense.query().whereRaw("TO_CHAR(expense_date, 'YYYY') = ?", [year]).whereRaw("TO_CHAR(expense_date, 'MM') = ?", [month])
+        const categories = categoriesQuery.map(category=>{
+            let categoryExpense = 0
 
-        let totalExpense = 0
+            for(let j = 0; j < category.expenses.length; j++){
+                    categoryExpense += parseFloat(category.expenses[j].amount.toString())
+            }
+            
+            return {
+                category: category.name,
+                expense: categoryExpense
+            }
+        })
+        
+        let expensesTotalAmount = 0
 
-        for(let i = 0; i < queryExpensesByYearAndMonth.length; i++){
-            totalExpense += parseFloat(queryExpensesByYearAndMonth[i].amount.toString())
+        for(let i = 0; i < categories.length; i++){
+            expensesTotalAmount += parseFloat(categories[i].expense.toString())
         }
 
-        const queryRevenuesByYearAndMonth = await Revenue.query().whereRaw("TO_CHAR(revenue_date, 'YYYY') = ?", [year]).whereRaw("TO_CHAR(revenue_date, 'MM') = ?", [month])
+        const revenues = await Revenue.query()
 
-        let totalRevenue = 0
+        let revenuesTotalAmount = 0
 
-        for(let i = 0; i < queryRevenuesByYearAndMonth.length; i++){
-            totalRevenue += parseFloat(queryRevenuesByYearAndMonth[i].amount.toString())
+        for(let i =0; i < revenues.length; i++){
+            revenuesTotalAmount += parseFloat(revenues[i].amount.toString())
         }
 
-        const finalBudget = totalRevenue - totalExpense
+        const finalBudget = revenuesTotalAmount - expensesTotalAmount 
 
-        return response.status(200).json({ totalExpense : totalExpense, totalRevenue : totalRevenue, finalBudget : finalBudget })
+        return response.status(200).json({
+            revenuesTotalAmount : revenuesTotalAmount,
+            expensesTotalAmount : expensesTotalAmount,
+            expensesByCategories : categories,
+            finalBudget : finalBudget
+        })
     }
 }
--- app/models/expenses_category.ts
+++ app/models/expenses_category.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import * as relations  from '@adonisjs/lucid/types/relations'
+import Expense from './expense.js'
 
 export default class ExpensesCategory extends BaseModel {
   @column({ isPrimary: true })
@@ -7,6 +9,9 @@ export default class ExpensesCategory extends BaseModel {
 
   @column()
   declare name: string
+
+  @hasMany(()=>Expense,{foreignKey:'id'})
+  declare expenses : relations.HasMany<typeof Expense>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
